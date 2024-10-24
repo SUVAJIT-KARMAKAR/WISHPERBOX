8553c7bfc461c32982275462e9fcdc2337948459
SUVAJITKARMAKAR <suvajit2001k@gmail.com>
2024-10-25 00:25:28 +0530

    SUVAJITKARMAKAR | WISHPERBOX | Schema > signInSchema completed

--- src/schema/signInSchema.ts
+++ src/schema/signInSchema.ts
@@ -1,2 +1,9 @@
 // src > signInSchema.ts
 // Importing the required modules in the work space 
+import { z } from "zod";
+
+// Schema validation for signIn
+export const signInSchema = z.object({
+    identifier : z.string(),
+    password : z.string()
+});
